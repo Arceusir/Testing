a220c86c23eea8c309c3465f240fc055451b8cb5
Arceusir <aaronchristopher.dalmacio@gmail.com>
2021-11-26 03:38:17 +0000

    New

--- midterm_app.sh
+++ midterm_app.sh
@@ -12,11 +12,11 @@ echo "FROM python" > tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  midterm_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "EXPOSE 5000" >> tempdir/Dockerfile
 echo "RUN python3 -m pip install flask" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/midterm_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t midterm_app .
-docker run -t -d -p 5050:5050 --name midterm_apprunning midterm_app
+docker run -t -d -p 5000:5000 --name midterm_apprunning midterm_app
 docker ps -a
